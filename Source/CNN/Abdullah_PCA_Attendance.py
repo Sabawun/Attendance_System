c2382d2a2f9596df9566ee6ed8e690765b854d82
Sabawun <67931515+Sabawun@users.noreply.github.com>
2022-04-07 07:13:15 +0300

    Add files via upload

---
+++ Source/CNN/Abdullah_PCA_Attendance.py
@@ -0,0 +1,38 @@
+import numpy as np
+from sklearn.decomposition import PCA
+import pandas as pd
+
+User = "Abdullah"
+
+Flatten_Norm_Testing_Images = np.load("/Users/sabawunafzalkhattak/Desktop/Attendance_System/Files/" + User +
+                                      "/" + User + "_Test_File.npy")
+Flatten_Norm_Training_Images = np.load("/Users/sabawunafzalkhattak/Desktop/Attendance_System/Files/" + User +
+                                       "/" + User + "_Train_File.npy")
+Training_Labels = np.load("/Users/sabawunafzalkhattak/Desktop/Attendance_System/Files/" + User +
+                          "/" + User + "_Train_Labels.npy")
+Testing_Labels = np.load("/Users/sabawunafzalkhattak/Desktop/Attendance_System/Files/" + User +
+                         "/" + User + "_Test_Labels.npy")
+
+feat_cols = ['pixel' + str(i) for i in range(Flatten_Norm_Training_Images.shape[1])]
+df_attendance = pd.DataFrame(Flatten_Norm_Training_Images, columns=feat_cols)
+
+df_attendance['label'] = Training_Labels
+
+pca_attendance = PCA(n_components=8)
+principalComponents_attendance = pca_attendance.fit_transform(df_attendance.iloc[:, :-1])
+
+principal_attendance_Df = pd.DataFrame(data=principalComponents_attendance,
+                                       columns=['principal component 1', 'principal component 2',
+                                                'principal component 3',
+                                                'principal component 4',
+                                                'principal component 5', 'principal component 6',
+                                                'principal component 7', 'principal component 8',
+                                                ])
+principal_attendance_Df['y'] = Training_Labels
+
+pca = PCA(0.95)
+
+pca.fit(Flatten_Norm_Training_Images)
+
+train_img_pca = pca.transform(Flatten_Norm_Training_Images)
+test_img_pca = pca.transform(Flatten_Norm_Testing_Images)
---
+++ Source/CNN/Onur_CNN_Model.py
@@ -0,0 +1,48 @@
+import numpy as np
+from keras.utils import np_utils
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout
+from Onur_PCA_Attendance import pca, train_img_pca, test_img_pca
+
+User = "Onur"
+
+Training_Labels = np.load("/Users/sabawunafzalkhattak/Desktop/Attendance_System/Files/" + User +
+                          "/" + User + "_Train_Labels.npy")
+Testing_Labels = np.load("/Users/sabawunafzalkhattak/Desktop/Attendance_System/Files/" + User +
+                         "/" + User + "_Test_Labels.npy")
+
+y_train = np_utils.to_categorical(Training_Labels)
+y_test = np_utils.to_categorical(Testing_Labels)
+
+batch_size = 12  # 186 photos
+num_classes = 2  # change to 4
+epochs = 150
+
+model = Sequential()
+model.add(Dense(1024, activation='relu', input_shape=(pca.n_components_,)))
+model.add(Dense(512, activation='relu'))
+model.add(Dropout(0.8))
+model.add(Dense(256, activation='relu'))
+model.add(Dropout(0.8))
+model.add(Dense(256, activation='relu'))
+model.add(Dropout(0.4))
+model.add(Dense(256, activation='relu'))
+model.add(Dense(256, activation='relu'))
+model.add(Dense(128, activation='relu'))
+model.add(Dropout(0.4))
+model.add(Dense(64, activation='relu'))
+model.add(Dense(32, activation='relu'))
+model.add(Dropout(0.4))
+model.add(Dense(16, activation='relu'))
+model.add(Dense(num_classes, activation='softmax'))
+
+model.summary()
+
+model.compile(loss='categorical_crossentropy',
+              optimizer='Adam',
+              metrics=['accuracy'])
+
+history = model.fit(train_img_pca, y_train, batch_size=batch_size, epochs=epochs,
+                    validation_data=(test_img_pca, y_test))
+
+model.save("/Users/sabawunafzalkhattak/Desktop/Attendance_System/CNN_Models/" + User)
