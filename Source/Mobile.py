81b586171d491fa182136ed4db35ee7f94ca5e98
Sabawun Afzal Khattak <khattak.sabawun@gmail.com>
2022-04-19 02:39:40 +0300

    Flow till detection

--- Source/Mobile.py
+++ Source/Mobile.py
@@ -3,8 +3,12 @@ from datetime import time
 import cv2
 import kivy
 import numpy as np
+from kivy.clock import Clock
 from kivy.lang import Builder
 from kivymd.app import MDApp
+from kivy.uix.image import Image
+from kivy.graphics.texture import Texture
+from kivy.core.window import Window
 from kivy.uix.label import Label
 from kivymd.uix.boxlayout import MDBoxLayout
 from kivy.uix.screenmanager import ScreenManager
@@ -13,12 +17,15 @@ from kivy.uix.camera import Camera
 from tensorflow import keras
 from Source.CNN.Sabawun_PCA_Attendance import pca
 
-
 CATEGORIES = ["Sabawun", "Other"]
 
 User = "Sabawun"
 
 model = keras.models.load_model("/Users/sabawunafzalkhattak/Desktop/Attendance_System/CNN_Models/" + User)
+
+classifier = cv2.CascadeClassifier("haarcascade_frontalface_alt.xml")
+
+Window.size = (320, 600)
 
 
 class Login(Screen):
@@ -33,34 +40,47 @@ class Course(Screen):
     pass
 
 
-classifier = cv2.CascadeClassifier("haarcascade_frontalface_alt.xml")
-
-
-def recognize(cam):
-    gray = cv2.cvtColor(cam, cv2.COLOR_BGR2GRAY)
-    faces = classifier.detectMultiScale(gray, 1.5, 5)
-    Image_test = []
-    for face in faces:
-        x, y, w, h = face
-        im_face = cam[y:y + h, x:x + w]  # frame with only detected face
-        new_array = cv2.resize(im_face, (64, 64))
-        Face_image = np.array(new_array)
-        Face_image = Face_image / 255.0
-        Face_image = Face_image.reshape(-1, 64, 64, 3)
-        Face_image_flat = Face_image.reshape(-1, 12288)
-        Face_image_pca = pca.transform(Face_image_flat)
-        Image_test.append(np.array(Face_image_pca))
-
-    for i, face in enumerate(faces):
-        predict_x = model.predict(np.array(Image_test[i]))
-        classes_x = CATEGORIES[int(np.argmax(predict_x, axis=1))]
-        print(classes_x)
-
-
 class Recognition(Screen):
-    def capture(self):
-        cam = self.ids['camera']
-        recognize(cam)
+    pass
+
+
+class KivyCamera(Image):
+
+    def __init__(self, fps=30, **kwargs):
+        super(KivyCamera, self).__init__(**kwargs)
+        self.capture = cv2.VideoCapture(0)
+        Clock.schedule_interval(self.update, 1.0 / fps)
+        self.actual_fps = []
+
+    def detect_faces(self, frame):
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+        faces = classifier.detectMultiScale(gray, 1.5, 5)
+
+        # Draw a rectangle around the faces
+        for (x, y, w, h) in faces:
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+        return frame
+
+    def frame_to_texture(self, frame):
+        # convert it to texture
+        buf1 = cv2.flip(frame, 0)
+        buf = buf1.tobytes()
+        image_texture = Texture.create(size=(frame.shape[1], frame.shape[0]), colorfmt='bgr')
+        image_texture.blit_buffer(buf, colorfmt='bgr', bufferfmt='ubyte')
+        return image_texture
+
+    def update(self, dt):
+        ret, frame = self.capture.read()
+        if not ret:
+            return
+
+        if self.detect:
+            self.detect_faces(frame)
+
+        # display image from the texture
+        self.texture = self.frame_to_texture(frame)
 
 
 class Manager(ScreenManager):
